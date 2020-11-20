ee3cd9a3b1be4c2c2bc09da5722763e0564ebd91
Fredrik Holmqvist <fredrik.holmqvist@gmail.com>
2020-11-20 20:33:32 +0100

    Setup post mmu virtual map, copied from x86_64
    I think this could be made general for EFI.
    
    Still missing is taking the current map and setting the virtual
    address for that region.
    Also removed x86_64 specific asm that seems kind of out of place

--- src/system/boot/platform/efi/arch/arm/arch_mmu.cpp
+++ src/system/boot/platform/efi/arch/arm/arch_mmu.cpp
@@ -3,8 +3,120 @@
  * Released under the terms of the MIT License.
  */
 
+
+#include <algorithm>
+
+#include <kernel.h>
+#include <arch_kernel.h>
+#include <boot/platform.h>
+#include <boot/stage2.h>
+#include <arch/x86/descriptors.h>
+
+#include <efi/types.h>
+#include <efi/boot-services.h>
+
+#include "mmu.h"
+#include "efi_platform.h"
+
+
 void
 arch_mmu_init()
 {
 	// Stub
+}
+
+
+// Called after EFI boot services exit.
+// Currently assumes that the memory map is sane... Sorted and no overlapping
+// regions.
+void
+arch_mmu_post_efi_setup(size_t memory_map_size,
+	efi_memory_descriptor *memory_map, size_t descriptor_size,
+	uint32_t descriptor_version)
+{
+	// Add physical memory to the kernel args and update virtual addresses for
+	// EFI regions.
+	addr_t addr = (addr_t)memory_map;
+	gKernelArgs.num_physical_memory_ranges = 0;
+
+	// First scan: Add all usable ranges
+	for (size_t i = 0; i < memory_map_size / descriptor_size; ++i) {
+		efi_memory_descriptor *entry
+			= (efi_memory_descriptor *)(addr + i * descriptor_size);
+		switch (entry->Type) {
+		case EfiLoaderCode:
+		case EfiLoaderData:
+		case EfiBootServicesCode:
+		case EfiBootServicesData:
+		case EfiConventionalMemory: {
+			// Usable memory.
+			// Ignore memory below 1MB and above 512GB.
+			uint64_t base = entry->PhysicalStart;
+			uint64_t end = entry->PhysicalStart + entry->NumberOfPages * 4096;
+			uint64_t originalSize = end - base;
+			if (base < 0x100000)
+				base = 0x100000;
+			if (end > (512ull * 1024 * 1024 * 1024))
+				end = 512ull * 1024 * 1024 * 1024;
+
+			gKernelArgs.ignored_physical_memory
+				+= originalSize - (max_c(end, base) - base);
+
+			if (base >= end)
+				break;
+			uint64_t size = end - base;
+
+			insert_physical_memory_range(base, size);
+			// LoaderData memory is bootloader allocated memory, possibly
+			// containing the kernel or loaded drivers.
+			if (entry->Type == EfiLoaderData)
+				insert_physical_allocated_range(base, size);
+			break;
+		}
+		case EfiACPIReclaimMemory:
+			// ACPI reclaim -- physical memory we could actually use later
+			break;
+		case EfiRuntimeServicesCode:
+		case EfiRuntimeServicesData:
+			entry->VirtualStart = entry->PhysicalStart;
+			break;
+		}
+	}
+
+	uint64_t initialPhysicalMemory = total_physical_memory();
+
+	// Second scan: Remove everything reserved that may overlap
+	for (size_t i = 0; i < memory_map_size / descriptor_size; ++i) {
+		efi_memory_descriptor *entry
+			= (efi_memory_descriptor *)(addr + i * descriptor_size);
+		switch (entry->Type) {
+		case EfiLoaderCode:
+		case EfiLoaderData:
+		case EfiBootServicesCode:
+		case EfiBootServicesData:
+		case EfiConventionalMemory:
+			break;
+		default:
+			uint64_t base = entry->PhysicalStart;
+			uint64_t end = entry->PhysicalStart + entry->NumberOfPages * 4096;
+			remove_physical_memory_range(base, end - base);
+		}
+	}
+
+	gKernelArgs.ignored_physical_memory
+		+= initialPhysicalMemory - total_physical_memory();
+
+	// Sort the address ranges.
+	sort_address_ranges(gKernelArgs.physical_memory_range,
+		gKernelArgs.num_physical_memory_ranges);
+	sort_address_ranges(gKernelArgs.physical_allocated_range,
+		gKernelArgs.num_physical_allocated_ranges);
+	sort_address_ranges(gKernelArgs.virtual_allocated_range,
+		gKernelArgs.num_virtual_allocated_ranges);
+
+	// Switch EFI to virtual mode, using the kernel pmap.
+	// Something involving ConvertPointer might need to be done after this?
+	// http://wiki.phoenix.com/wiki/index.php/EFI_RUNTIME_SERVICES
+	kRuntimeServices->SetVirtualAddressMap(memory_map_size, descriptor_size,
+		descriptor_version, memory_map);
 }
