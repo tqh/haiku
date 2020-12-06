1ddddbf6993ed1dc4e5494355068fb2af22b7d11
Fredrik Holmqvist <fredrik.holmqvist@gmail.com>
2020-12-06 16:13:15 +0100

    Cleanup ARM arch_start (starting to look generic)

--- src/system/boot/platform/efi/arch/arm/arch_start.cpp
+++ src/system/boot/platform/efi/arch/arm/arch_start.cpp
@@ -24,74 +24,48 @@ arch_start_kernel(addr_t kernelEntry)
 	// Prepare to exit EFI boot services.
 	// Read the memory map.
 	// First call is to determine the buffer size.
-	size_t memory_map_size = 0;
-	efi_memory_descriptor dummy;
-	efi_memory_descriptor *memory_map;
+	size_t mmap_size = 0;
 	size_t map_key;
 	size_t descriptor_size;
 	uint32_t descriptor_version;
-	if (kBootServices->GetMemoryMap(&memory_map_size, &dummy, &map_key,
-			&descriptor_size, &descriptor_version) != EFI_BUFFER_TOO_SMALL) {
+	if (kBootServices->GetMemoryMap(&mmap_size, NULL, &map_key,
+			&descriptor_size, &descriptor_version) != EFI_BUFFER_TOO_SMALL)
 		panic("Unable to determine size of system memory map");
-	}
+
 
 	// Allocate a buffer twice as large as needed just in case it gets bigger
 	// between calls to ExitBootServices.
-	size_t actual_memory_map_size = memory_map_size * 2;
-	memory_map
-		= (efi_memory_descriptor *)kernel_args_malloc(actual_memory_map_size);
+	size_t allocated_mmap_size = mmap_size * 2;
+	efi_memory_descriptor *mmap =
+		(efi_memory_descriptor *)kernel_args_malloc(allocated_mmap_size);
 
-	if (memory_map == NULL)
+	if (mmap == NULL)
 		panic("Unable to allocate memory map.");
 
-	// Read (and print) the memory map.
-	memory_map_size = actual_memory_map_size;
-	if (kBootServices->GetMemoryMap(&memory_map_size, memory_map, &map_key,
-			&descriptor_size, &descriptor_version) != EFI_SUCCESS) {
-		panic("Unable to fetch system memory map.");
-	}
+	dprintf("Calling ExitBootServices. So long, EFI!\n");
+	do {
+		mmap_size = allocated_mmap_size;
+		if (kBootServices->GetMemoryMap(&mmap_size, mmap, &map_key,
+				&descriptor_size, &descriptor_version) != EFI_SUCCESS)
+			panic("Unable to fetch system memory map.");
 
-	addr_t addr = (addr_t)memory_map;
-	dprintf("System provided memory map:\n");
-	for (size_t i = 0; i < memory_map_size / descriptor_size; ++i) {
-		efi_memory_descriptor *entry
-			= (efi_memory_descriptor *)(addr + i * descriptor_size);
+	} while (kBootServices->ExitBootServices(kImage, map_key) != EFI_SUCCESS);
+	// The console was provided by boot services, disable it.
+	stdout = NULL;
+	stderr = NULL;
+
+	dprintf("Memory map after exit:\n");
+	addr_t addr = (addr_t)mmap;
+	for (size_t offset = 0; offset < mmap_size; offset += descriptor_size) {
+		efi_memory_descriptor *entry = (efi_memory_descriptor *)(addr + offset);
 		dprintf("  %#lx-%#lx  %#lx %#x %#lx\n", entry->PhysicalStart,
 			entry->PhysicalStart + entry->NumberOfPages * B_PAGE_SIZE,
 			entry->VirtualStart, entry->Type, entry->Attribute);
 	}
 
-	// Attempt to fetch the memory map and exit boot services.
-	// This needs to be done in a loop, as ExitBootServices can change the
-	// memory map.
-	// Even better: Only GetMemoryMap and ExitBootServices can be called after
-	// the first call to ExitBootServices, as the firmware is permitted to
-	// partially exit. This is why twice as much space was allocated for the
-	// memory map, as it's impossible to allocate more now.
-	// A changing memory map shouldn't affect the generated page tables, as
-	// they only needed to know about the maximum address, not any specific
-	// entry.
-	dprintf("Calling ExitBootServices. So long, EFI!\n");
-	while (true) {
-		if (kBootServices->ExitBootServices(kImage, map_key) == EFI_SUCCESS) {
-			// The console was provided by boot services, disable it.
-			stdout = NULL;
-			stderr = NULL;
-			// Can we adjust gKernelArgs.platform_args.serial_base_ports[0]
-			// to something fixed in qemu for debugging?
-			break;
-		}
-
-		memory_map_size = actual_memory_map_size;
-		if (kBootServices->GetMemoryMap(&memory_map_size, memory_map, &map_key,
-				&descriptor_size, &descriptor_version) != EFI_SUCCESS) {
-			panic("Unable to fetch system memory map.");
-		}
-	}
-
 	// Update EFI, generate final kernel physical memory map, etc.
-	arch_mmu_post_efi_setup(memory_map_size, memory_map,
-			descriptor_size, descriptor_version);
+	arch_mmu_post_efi_setup(mmap_size, mmap, descriptor_size,
+		descriptor_version);
 
 	//smp_boot_other_cpus(final_pml4, kernelEntry);
 
